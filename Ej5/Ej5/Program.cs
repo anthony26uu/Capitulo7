553c578ada28273ad94a836f19a43ad3761d2e3e
Anthony Javier Santana Polanco <anthony26uu@gmail.com>
2017-01-14 02:24:36 -0400

    Modificacion

---
+++ Ej5/Ej5/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Ej5
+{
+    class Program
+    {
+        static void Main()
+        {
+
+           Queue agenda = new Queue();
+            string tlf;
+            string nombre;
+                int lista = 1;
+                while (lista == 1)
+                {
+
+                Console.WriteLine("Ingrese nombre de persona que desea agregar a contactos ");
+               nombre= Console.ReadLine();
+                agenda.Enqueue(nombre);
+                Console.WriteLine("ingrese tlf ");
+                tlf = Console.ReadLine();
+                 agenda.Enqueue(tlf);
+
+                    Console.WriteLine("Coloque 0 si no ingresaran mas personas de lo contrario ingrese 1 ");
+                    lista = int.Parse(Console.ReadLine());
+
+
+                }
+                while (agenda.Count > 0)
+                    // Thread.Sleep(2000);
+                    Console.WriteLine("Persona: {0} :: Numero {1}" , agenda.Dequeue(), agenda.Dequeue());
+                
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
